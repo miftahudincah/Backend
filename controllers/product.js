3192b199874d8a1d9e84bb45b6904da44bdafdb5
miftahudincah <zaki5go@gmail.com>
2024-06-30 13:16:36 +0700

    Initial commit

---
+++ controllers/product.js
@@ -0,0 +1,25 @@
+const Product = require('../models/product.js');
+
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.createProduct = async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    });
+
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');  // Tambahkan ini
+const productRoutes = require('./routes/produk.js');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());  // Tambahkan ini
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error(err);
+});
+
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
